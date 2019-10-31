9cec7fc16a09e9bd5d24c02f0b898faa20c3323b
xiwang wang <simon9211@126.com>
2019-10-31 14:13:07 +0800

    add http service

--- Sources/swift_service/main.swift
+++ Sources/swift_service/main.swift
@@ -1,1 +1,20 @@
 print("Hello, world!")
+import PerfectHTTP
+import PerfectHTTPServer
+
+var routes = Routes()
+routes.add(method: .get, uri: "/") {
+    request, response in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+        .completed()
+}
+
+do {
+    // 启动HTTP服务器
+    try HTTPServer.launch(
+        .server(name: "www.example.ca", port: 8181, routes: routes))
+} catch {
+    fatalError("\(error)") // fatal error launching one of the servers
+}
+
